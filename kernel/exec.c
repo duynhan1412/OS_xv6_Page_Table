1fd39f53d5dba9f6555eed7c1b5ee02877105c62
duynhan1412 <trinhduynhan0506@gmail.com>
2026-04-15 20:37:45 +0700

    Bai 1

--- kernel/exec.c
+++ kernel/exec.c
@@ -128,6 +128,10 @@ exec(char *path, char **argv)
   p->trapframe->sp = sp; // initial stack pointer
   proc_freepagetable(oldpagetable, oldsz);
 
+  // neu la process 1 thi in ra cau truc bang phan trang
+  if(p->pid == 1)
+    vmprint(pagetable);
+
   return argc; // this ends up in a0, the first argument to main(argc, argv)
 
  bad:
